d3efa4452b31769e5fbddaa37f01bb57027ce719
Carlos <21000405@alumnos.utleon.edu.mx>
2025-06-18 19:30:06 -0600

    Se crea el archivo SumaDosNumeros.kt

--- app/src/main/java/com/example/ejemplo2composeidgs901/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo2composeidgs901/MainActivity.kt
@@ -1,31 +1,90 @@
 package com.example.ejemplo2composeidgs901
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import com.example.ejemplo2composeidgs901.ui.theme.Ejemplo2ComposeIDGS901Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
-            Ejemplo2ComposeIDGS901Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+//            val navController = rememberNavController()
+//            NavHost(navController=navController, startDestination = "inicio"){
+//                composable ("inicio"){
+//                    PantallaInicio(navController)
+//                }
+//                composable ("detalle/{nombre}"){
+//                        backStackEntry ->
+//                    val nombre = backStackEntry.arguments?.getString("nombre") ?: "Invitado"
+//                    PantallaDetalle(navController, nombre)
+//                }
+//            }
+            sumaDosNumeros()
+        }
+    }
+}
+
+@Composable
+fun PantallaInicio(navController: NavHostController) {
+    var nombre by remember { mutableStateOf("") }
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Pantalla de inicio")
+        Spacer(modifier = Modifier.size(16.dp))
+        TextField(
+            value = nombre,
+            onValueChange = { nombre = it },
+            label = { Text("Nombre") },
+
+            modifier= Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.size(16.dp))
+        Button(onClick = {
+            val nombre=nombre.ifBlank { "Invitado" }
+            navController.navigate("detalle/$nombre")
+
+        })  {
+            Text("Ingresar")
+        }
+    }
+}
+
+@Composable
+fun PantallaDetalle(navController: NavHostController, nombre: String){
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Hola $nombre")
+        Button(onClick = {
+            navController.navigate("inicio")
+        }){
+            Text("Volver")
         }
     }
 }
